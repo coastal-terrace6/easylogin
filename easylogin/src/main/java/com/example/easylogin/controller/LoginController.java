040d34d30881befee9d6d667125d9705fbebf2d5
coastal-terrace6 <jobget2021@gmail.com>
2021-07-29 22:42:00 +0900

    ログイン

---
+++ easylogin/src/main/java/com/example/easylogin/controller/LoginController.java
@@ -0,0 +1,63 @@
+package com.example.easylogin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.easylogin.model.dao.UserRepository;
+import com.example.easylogin.model.entity.User;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	UserRepository userRepos;
+	
+	//ローカルホストにアクセスがあった場合起動
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	//ローカルホスト”ログイン”にアクセスがあった場合起動
+	@RequestMapping("/login")
+	public String login(
+			
+			//クライアントからのリクエスト
+			@RequestParam("userName") String userName,
+			@RequestParam("password") String password,
+			//クライアント側に返す為のオブジェクト
+			Model m)
+	{
+		String message = "Welcome!";
+		
+		List<User> users = userRepos.findByUserNameAndPassword(userName, password);
+		if(users.size() > 0) {
+			User user = users.get(0);
+			message += user.getFullName();
+		}else {
+			message += "guest";
+		}
+		m.addAttribute("message", message);
+		
+		return "login";
+	}
+	
+/*	@RequestMapping("/")
+	@ResponseBody
+	public String showUsers() {
+		List<User> users = userRepos.findAll();
+		
+		User user = users.get(0);
+		
+		String info = user.getUserName() + " " + user.getPassword();
+		
+		return info;
+	}*/
+	
+}
